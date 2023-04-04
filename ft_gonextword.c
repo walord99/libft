b6f4b9f6c1dc6e87c4eb525c55bc5dd67c923771
Walord <benplante99@gmail.com>
2023-04-04 13:11:44 -0400

    final formating touches

--- ft_gonextword.c
+++ ft_gonextword.c
@@ -6,7 +6,7 @@
 /*   By: Ben <benplante99@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 12:59:58 by Ben               #+#    #+#             */
-/*   Updated: 2023/04/04 13:01:13 by Ben              ###   ########.fr       */
+/*   Updated: 2023/04/04 13:11:25 by Ben              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	ft_go_next_word(const char *str, char delemiter)
 {
-	size_t i;
-	int next_word_ready;
+	int		next_word_ready;
+	size_t	i;
 
 	i = 0;
 	next_word_ready = 0;

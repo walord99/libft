1f089d2dff485ed0e114ace4847fcf8bd0dc037c
Walord <benplante99@gmail.com>
2023-10-13 21:47:57 -0400

    list fixes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: walord <walord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 13:51:05 by bplante           #+#    #+#             */
-/*   Updated: 2023/10/13 21:44:02 by walord           ###   ########.fr       */
+/*   Updated: 2023/10/13 21:46:50 by walord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ t_list				*ft_lstadd_front(t_list *list, void *content);
 t_list				*ft_lstlast(t_list *lst);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstadd_back(t_list *lst, void *content);
-t_list				*ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
 t_list				*ft_lstclear(t_list *lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
 t_list				*ft_lstmap(t_list *lst, void *(*f)(void *));
--- src/ft_lstmap_bonus.c
+++ src/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: walord <walord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 12:49:13 by Ben               #+#    #+#             */
-/*   Updated: 2023/10/13 21:32:13 by walord           ###   ########.fr       */
+/*   Updated: 2023/10/13 21:47:29 by walord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *))
 	new_lst = NULL;
 	while (lst)
 	{
-		ft_lstadd_back(&new_lst, (*f)(lst->content));
+		ft_lstadd_back(new_lst, (*f)(lst->content));
 		lst = lst->next;
 	}
 	return (new_lst);

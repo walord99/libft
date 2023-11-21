cc7e16b93aaf012f502ff8824fba8d09071f1eb8
Walord <benplante99@gmail.com>
2023-11-21 08:41:58 -0500

    del == NULL fix

--- src/lst/ft_lst_destory_nodes.c
+++ src/lst/ft_lst_destory_nodes.c
@@ -6,7 +6,7 @@
 /*   By: bplante <bplante@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 08:16:02 by bplante           #+#    #+#             */
-/*   Updated: 2023/11/21 08:17:34 by bplante          ###   ########.fr       */
+/*   Updated: 2023/11/21 08:22:17 by bplante          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,5 @@
 
 void	lst_destroy_nodes(t_list *list)
 {
-	t_list	*temp;
-
-	while (list)
-	{
-		temp = list->next;
-		free(list);
-		list = temp;
-	}
+	ft_lstclear(list, NULL);
 }
--- src/lst/ft_lstdelone_bonus.c
+++ src/lst/ft_lstdelone_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: walord <walord@student.42.fr>              +#+  +:+       +#+        */
+/*   By: bplante <bplante@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 12:49:08 by Ben               #+#    #+#             */
-/*   Updated: 2023/10/13 21:46:01 by walord           ###   ########.fr       */
+/*   Updated: 2023/11/21 08:21:55 by bplante          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	(*del)(lst->content);
+	if (del)
+		(*del)(lst->content);
 	free(lst);
 }
